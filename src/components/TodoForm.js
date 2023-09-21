31195836ad23c07312dbd500a98dd4b86c8304ea
Xinyu <zhuxinyu1997@gmail.com>
2023-09-21 23:11:58 +1200

    'upload'

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>
+  <form>
+    <input
+      type='text'
+      autoFocus
+      className="new-todo"
+      placeholder="What needs to be done?" />
+  </form>
